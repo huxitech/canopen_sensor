403abcfe1b90d2f8f926dcf631b5ef6872514252
xielonghua <admin@xielonghua.cn>
2022-03-04 12:09:43 +0800

    调整定时器精度

--- APP/bsp_port.c
+++ APP/bsp_port.c
@@ -183,8 +183,8 @@ uint8_t bsp_TimerOpen(void){
 	NVIC_Init(&NVIC_InitStructure);
 
 	/* Compute the prescaler value */
-	/* TIM3 TICK = 84M  Set  1 TICK = 10us */
-	uint16_t PrescalerValue = (uint16_t) (SystemCoreClock   / 50000) - 1;
+	/* TIM3 TICK = 84M  10KHZ Set  1 TICK = 100us */
+	uint16_t PrescalerValue = (uint16_t) (8400) - 1;
 
 	/* Time base configuration */
 	TIM_TimeBaseStructure.TIM_Period = 65535;
