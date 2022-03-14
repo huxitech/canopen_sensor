4b713c2f205ba08673530e928a0769da70d8c29d
xielonghua <admin@xielonghua.cn>
2022-03-14 21:17:54 +0800

    定时器最大值配置传入

--- APP/main.c
+++ APP/main.c
@@ -6,8 +6,8 @@ int main(){
 	//SCB->VTOR = FLASH_BASE | 0x10000;
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
   delay_init(168);   
-	//node=3  baud=250K  tick=100us
-	canopen_Init(3,250000,100);
+	//node=3  baud=250K  tick=100us timer period = 65535
+	canopen_Init(3,250000,100,65535);
 	
 	LED_Init();
 	while(1)
